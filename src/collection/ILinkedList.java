68de243a8648b909df56d6f3c228007f121dcb68
alejandro945 <alejo8677@gmail.com>
2021-09-03 20:36:19 -0500

    Decoupling

---
+++ src/collection/ILinkedList.java
@@ -0,0 +1,10 @@
+package collection;
+
+public interface ILinkedList<T> {
+   public String addNode(T node);
+   public String removeNode(int pos);
+   public String editNode(int pos,T node);
+   public T searchNode(int pos);
+   public boolean isEmpty();
+   public int size();
+}
